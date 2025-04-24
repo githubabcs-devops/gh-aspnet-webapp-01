c6f4d201594a0206b1f884b4a6c31d5397b7be4d
Calin Lupas <lupas_calin@yahoo.com>
2025-04-24 16:00:10 -0400

    Add admin user details and logging in PrivacyModel
    Introduced a new `adminUserName` variable and a constant `DEFAULT_PASSWORD` in `Privacy.cshtml.cs`. Updated the `OnGet` method to handle a "drive" query parameter, construct a disk space command, and log the command string along with the admin username.

--- src/webapp01/Pages/Privacy.cshtml.cs
+++ src/webapp01/Pages/Privacy.cshtml.cs
@@ -7,6 +7,12 @@ public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
+    string adminUserName = "demouser@example.com";
+
+    // TODO: Don't use this in production
+    public const string DEFAULT_PASSWORD = "Pass@word1";
+
+
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
         _logger = logger;
@@ -14,6 +20,10 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
+        string drive = Request.Query.ContainsKey("drive") ? Request.Query["drive"] : "C";
+        var str = $"/C fsutil volume diskfree {drive}:";
+        _logger.LogInformation($"Command str: {str}");
+        _logger.LogInformation("Admin" + adminUserName);
     }
 }
 
