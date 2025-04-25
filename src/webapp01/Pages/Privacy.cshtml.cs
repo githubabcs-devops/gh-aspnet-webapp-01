c300100bac0f7310216e087b8ad9a5c2d60ebfa0
Calin Lupas <lupas_calin@yahoo.com>
2025-04-25 09:28:33 -0400

    Add admin user logging and default password in PrivacyModel

--- src/webapp01/Pages/Privacy.cshtml.cs
+++ src/webapp01/Pages/Privacy.cshtml.cs
@@ -7,6 +7,11 @@ public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
+    string adminUserName = "demouser@example.com";
+
+    // TODO: Don't use this in production
+    public const string DEFAULT_PASSWORD = "Pass@word1";
+
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
         _logger = logger;
@@ -14,6 +19,12 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
+        string drive = Request.Query.ContainsKey("drive") ? Request.Query["drive"] : "C";
+        var str = $"/C fsutil volume diskfree {drive}:";
+        _logger.LogInformation($"Command str: {str}");
+        _logger.LogInformation("Admin" + adminUserName);
+        _logger.LogInformation($"User: {User.Identity?.Name}");
+        _logger.LogInformation($"Admin: {User.IsInRole("Admin")}");
     }
 }
 
